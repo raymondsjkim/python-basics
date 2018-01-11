bdaa26829bfd1d198aa1a0945704f83d9f739fba
Raymond Kim <raymond.sj.kim@gmail.com>
2018-01-11 14:44:54 -0800

    Update assignmentOne.py

--- assignmentOne.py
+++ assignmentOne.py
@@ -1,15 +1,16 @@
 '''
 Raymond Kim
 01/11/2018
-
 Python Assignment 1
 '''
 
+#Get user input
 number = input("Enter an integer between 6 and 24: ")
+#Convert str to int
 number = int(number)
 
 if number < 6 or number > 24:
-    print("This is the case and nothing further");
+    print("This is the case");
 elif 6 < number <= 12:
     print("Congratulations")
 else:
